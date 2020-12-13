e3efbe9a3f2d7650af945843311067db43c37913
Jelena Beader <jelenabeader.sm@gmail.com>
2020-12-13 22:47:18 +0100

    Otvaranje komunikacije na Monitoring serveru

---
+++ MonitoringServer/LogService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringServer
+{
+    public class LogService : IMonitoringServer
+    {
+        public void LogCommunication(string sender, string receiver, string timestamp, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogCommunicationEnd(string sender, string receiver, string timestamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogCommunicationStart(string sender, string receiver, string timestamp)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ MonitoringServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/Monitoring";
+            ServiceHost host = new ServiceHost(typeof(LogService));
+            host.AddServiceEndpoint(typeof(IMonitoringServer), binding, address);
+            
+            try
+            {
+                host.Open();
+                Console.WriteLine("MonitoringServer is started.\nPress <enter> to stop ...");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERROR] {0}", e.Message);
+                Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+            }
+            finally
+            {
+                host.Close();
+            }
+
+
+        }
+    }
+}
