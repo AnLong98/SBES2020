6fddf8d98aa3d404567dc80e1c60a9c1b179fd82
stefan <stefanbesovic@gmail.com>
2020-12-14 16:46:29 +0100

    Setup WCF Service

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,21 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/Service";
+
+            /*binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;*/
+
+            ServiceHost host = new ServiceHost(typeof(WCFService));
+            host.AddServiceEndpoint(typeof(ICentralAuthServer), binding, address);
+
+            host.Open();
+
+            Console.WriteLine("Servis je pokrenut.");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Service/WCFService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class WCFService : ICentralAuthServer
+    {
+        public void Authenticate(string ip, string port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RevocateCertificate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
