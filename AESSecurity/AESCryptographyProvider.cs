9e2116671aca4c3e3f4466538f19704bc68be6c6
Predrag <svetoizvorac@yahoo.com>
2020-12-18 18:58:01 +0100

    Working on AES

---
+++ AESSecurity/AESCryptographyProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESSecurity
+{
+    public class AESCryptographyProvider : IAESSecurity
+    {
+        public string Decrypt(string cyphertext, string key)
+        {
+            byte[] encryptedByteArray = Convert.FromBase64String(cyphertext);
+            byte[] decryptedByteArray = null;
+
+            AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.None
+            };
+
+            aesCryptoProvider.IV = encryptedByteArray.Take(aesCryptoProvider.BlockSize / 8).ToArray();
+
+            ICryptoTransform cryptoTransform = aesCryptoProvider.CreateDecryptor();
+
+            using (MemoryStream memoryStream = new MemoryStream(encryptedByteArray.Skip(aesCryptoProvider.BlockSize / 8).ToArray()))
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Read))
+                {
+                    decryptedByteArray = new byte[encryptedByteArray.Length - aesCryptoProvider.BlockSize / 8];
+                    cryptoStream.Read(decryptedByteArray, 0, decryptedByteArray.Length);
+                }
+            }
+
+            return Convert.ToBase64String(decryptedByteArray);
+        }
+
+        public string Encrypt(string plaintext, string key)
+        {
+            byte[] encryptedByteArray;
+            byte[] plaintextByteArray = Convert.FromBase64String(plaintext);
+
+            AesCryptoServiceProvider aesCryptoProvider = new AesCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.None
+            };
+
+            aesCryptoProvider.GenerateIV(); //create init vector at random
+
+            ICryptoTransform cryptoTransform = aesCryptoProvider.CreateEncryptor();
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(plaintextByteArray, 0, plaintext.Length);
+                    encryptedByteArray = aesCryptoProvider.IV.Concat(memoryStream.ToArray()).ToArray();    //encrypted image body with IV
+                }
+            }
+
+            return Convert.ToBase64String(encryptedByteArray);
+        }
+    }
+}
---
+++ AESSecurityTests/AESCryptoTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESSecurity.Tests
+{
+    [TestFixture]
+    public class AESCryptoTests
+    {
+        [TestCase("Cao ja samo testiram ovo", "FFDAFA2FFBA91E5A6E50197AABC0624A")]
+        public void Encrypt__Plaintext__AssertPlaintextChanged(string plaintext, string key)
+        {
+            AESCryptographyProvider provider = new AESCryptographyProvider();
+            string encrypted = provider.Encrypt(plaintext, key);
+
+            Assert.AreNotEqual(plaintext, encrypted);
+        }
+
+        [TestCase("Cao ja samo testiram ovo", "FFDAFA2FFBA91E5A6E50197AABC0624A")]
+        public void Decyrpt__PlaintextEncrypted__AssertDecryptedToPlaintext(string plaintext, string key)
+        {
+            AESCryptographyProvider provider = new AESCryptographyProvider();
+            string encrypted = provider.Encrypt(plaintext, key);
+            string decrypted = provider.Decrypt(encrypted, key);
+
+            Assert.AreEqual(plaintext, decrypted);
+        }
+
+    }
+}
