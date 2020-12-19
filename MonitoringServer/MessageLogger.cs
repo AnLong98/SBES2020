a831106d79d30bb449d6c15e3ed41a303a1a3d2c
Jelena Beader <jelenabeader.sm@gmail.com>
2020-12-19 20:05:02 +0100

    Added message logger

---
+++ MonitoringServer/MessageLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringServer
+{
+    public class MessageLogger
+    {
+        public static void LoggMessage(string s)
+        {
+            string path = System.IO.Path.GetFullPath("../../Messages/messages.txt");
+            FileStream stream = new FileStream(path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(stream);
+
+            sw.WriteLine(s);
+
+            sw.Close();
+            stream.Close();
+
+        }
+        
+    }
+}
