80041504cf4b4fdf034b928d2dd6fcffef7e2b22
stefan <stefanbesovic@gmail.com>
2020-12-22 14:55:50 +0100

    implemented method for getting all users, added parser

--- Service/WCFService.cs
+++ Service/WCFService.cs
@@ -5,6 +5,8 @@ using System.Security.Principal;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Common;
+using Common.Parsers;
 
 namespace Service
 {
@@ -14,7 +16,7 @@ namespace Service
         {
             IIdentity identity = Thread.CurrentPrincipal.Identity;
             WindowsIdentity windowsIdentity = identity as WindowsIdentity;
-            string userName = windowsIdentity.Name;
+            string userName = WinLogonNameParser.ParseName(windowsIdentity.Name);
 
             if (!Users.UserAccounts.ContainsKey(userName))
                 Users.UserAccounts.Add(userName, new User(ip, port, userName));
@@ -22,7 +24,20 @@ namespace Service
 
         public List<User> GetAllUsers()
         {
-            throw new NotImplementedException();
+            List<User> users = new List<User>();
+            IIdentity id = Thread.CurrentPrincipal.Identity;
+            WindowsIdentity winId = id as WindowsIdentity;
+            string userName = WinLogonNameParser.ParseName(winId.Name);
+
+            foreach(KeyValuePair<string, User> kp in Users.UserAccounts) 
+            {
+                if(!kp.Key.Equals(userName)) //added all clients to the list / except the client we are sending this list to.
+                {
+                    users.Add(kp.Value);
+                }
+            }
+
+            return users;
         }
 
         public void RevocateCertificate()
